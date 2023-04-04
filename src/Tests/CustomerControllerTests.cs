35e618741f681e322f556a40a6999fa91397361e
Joao Oliveira <joao.bcoliveira@outlook.com>
2023-04-04 16:38:24 +0100

    Basic Tests on Get, Post, Put and Delete Methods

--- src/Tests/CustomerControllerTests.cs
+++ src/Tests/CustomerControllerTests.cs
@@ -20,17 +20,17 @@ namespace Tests
         }
 
         [Test]
-        public void Get_ActionExecutes_ReturnsOkResult()
+        public void Get_ActionExecutes_ReturnsOKStatusCodeResult()
         {
             //Arrange
 
             //Act
             var result = _controller.Get();
-            var okResult = result as OkObjectResult;
-
+            var statusCodeResult = result as OkObjectResult;
+            
             //Assert
-            Assert.IsNotNull(okResult);
-            Assert.That(okResult.StatusCode,Is.EqualTo(200) );
+            Assert.IsNotNull(statusCodeResult);
+            Assert.That(statusCodeResult.StatusCode,Is.EqualTo(200) );
 
         }
 
@@ -42,41 +42,137 @@ namespace Tests
            
             //Act
             var result = _controller.Get(id);
-            var okResult = result as NotFoundResult;
+            var statusCodeResult = result as NotFoundResult;
 
             //Assert
-            Assert.IsNotNull(okResult);
-            Assert.That(okResult.StatusCode, Is.EqualTo(404));
+            Assert.IsNotNull(statusCodeResult);
+            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(404));
 
         }
 
         [Test]
         [TestCase(1)]
-        public void Get_WithIDFilledExecutes_ReturnsOkResult(int id)
+        public void Get_WithIDFilledExecutes_ReturnsOkStatusCodeResult(int id)
         {
             //Arrange
 
             //Act
             var result = _controller.Get(id);
-            var okResult = result as OkObjectResult;
+            var statusCodeResult = result as OkObjectResult;
 
             //Assert
-            Assert.IsNotNull(okResult);
-            Assert.That(okResult.StatusCode, Is.EqualTo(200));
+            Assert.IsNotNull(statusCodeResult);
+            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(200));
 
         }
 
+        [Test]
+        [TestCaseSource(nameof(CustomersForPost))]
+        public void Post_WhenCalled_ShouldCreateARegistry(CustomerViewModel customer)
+        {
+            //Arrange
 
-        public static object[] CustomerViewModelWithNullValuesSource =
+            //Act
+            var result = _controller.Post(customer);
+            var statusCodeResult = result as OkObjectResult;
+
+            //Assert
+            Assert.IsNotNull(statusCodeResult);
+            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(200));
+
+        }
+
+        [Test]
+        public void Post_WhenCalledWithWrongEmailFormat_ShouldShowErrorMessageAndBadRequest()
+        {
+            //Arrange
+            var customer = new CustomerViewModel { Email = "joao@@joao.com", FirstName = "Joao", Password = "pwd", SurName = "Oliveira" };
+
+            //Act
+            var result = _controller.Post(customer);
+            var statusCodeResult = result as BadRequestObjectResult;
+
+            //Assert
+            Assert.IsNotNull(statusCodeResult);
+            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(400));
+            Assert.That(statusCodeResult.Value, Is.EqualTo("Error. Some input is missing or it is in wrong format."));
+
+        }
+
+        [Test]
+        public void Put_WhenCalled_ShouldCreateARegistry()
+        {
+            //Arrange
+            var customer = new CustomerViewModel { Id = 1, Email = "joao@@joao.com", FirstName = "Joao", Password = "pwd", SurName = "Oliveira" };
+            //Act
+            var result = _controller.Put(customer.Id,customer);
+            var statusCodeResult = result as OkObjectResult;
+
+            //Assert
+            Assert.IsNotNull(statusCodeResult);
+            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(200));
+
+        }
+
+        [Test]
+        public void Put_WhenCalledWithWrongEmailFormat_ShouldShowErrorMessageAndBadRequest()
+        {
+            //Arrange
+            var customer = new CustomerViewModel { Id = 1, Email = "joao@@joao.com", FirstName = "Joao", Password = "pwd", SurName = "Oliveira" };
+
+            //Act
+            var result = _controller.Put(customer.Id, customer);
+            var statusCodeResult = result as BadRequestObjectResult;
+
+            //Assert
+            Assert.IsNotNull(statusCodeResult);
+            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(400));
+            Assert.That(statusCodeResult.Value, Is.EqualTo("Error. Some input is missing or it is in wrong format."));
+
+        }
+
+        [Test]
+        [TestCase(0)]
+        public void Delete_WithIDFilledExecutes_ReturnsNotFoundResult(int id)
+        {
+            //Arrange
+
+            //Act
+            var result = _controller.Delete(id);
+            var statusCodeResult = result as NotFoundResult;
+
+            //Assert
+            Assert.IsNotNull(statusCodeResult);
+            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(404));
+
+        }
+
+        [Test]
+        [TestCase(1)]
+        public void Delete_WithIDFilledExecutes_ReturnsOkStatusCodeResult(int id)
+        {
+            //Arrange
+
+            //Act
+            var result = _controller.Delete(id);
+            var statusCodeResult = result as OkObjectResult;
+
+            //Assert
+            Assert.IsNotNull(statusCodeResult);
+            Assert.That(statusCodeResult.StatusCode, Is.EqualTo(200));
+
+        }
+
+        private static object[] CustomersWithNullValuesSource =
         {
             new CustomerViewModel{ Email = null, FirstName = "Joao"},
             new CustomerViewModel{ Email = "joao@joao.com", FirstName = "Joao",Password = "pwd",SurName =null},
-            new CustomerViewModel{ Email = "joao@joao.com", FirstName = null,Password = "pwd",SurName ="Oliveira"},
-            new CustomerViewModel{ Email = "joao@joao.com", FirstName = "Joao",Password = null,SurName ="Oliveira"},
             new CustomerViewModel()
-            
+        };
 
- 
+        private static object[] CustomersForPost =
+{
+            new CustomerViewModel{ Email = "joao@joao.com", FirstName = "Joao",Password = "pwd",SurName ="Oliveira"},
         };
     }
 }
